41e5131dfcb1dcb7678e3a976fbfc6dab69763e9
moollyy <136740176+moollyy@users.noreply.github.com>
2025-09-29 18:23:40 +0000

    hrr

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main
   public static void main(String[] args)
   {
     // write your code here
+    // lastFirstN("koolaid", "water", 3);
+
+    stringManip("pizza ", "sleep");
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,6 +13,15 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
+    // String output;
+    // String firstN; 
+    // String lastN;
+    // // to do, assign values to firstN and lastN that solves the problem 
+
+    // output = lastN + firstN;
+    // System.out.println(output);
+
+
   
   }
 
@@ -17,6 +29,28 @@ public class Main
   */
   public static void stringManip(String s1, String s2)
   {
+    
+    // String sUpper;
+    // String sL;
+
+    String Up = s1.toUpperCase();
+
+
+    String Reg0 = s2;
+    // String Reg = Reg.substring(0, 1).toUpperCase();
+    String firstLetter = Reg0.substring(0, 1);  // first letter of 2nd word
+    String firstLetterCAPS = firstLetter.toUpperCase(); 
+    String everythingAfter = s2.substring(1);  // everything AFTER the 1st letter
+    everythingAfter = everythingAfter.toLowerCase();
+    Reg0 = firstLetter + everythingAfter;
+   
+
+    String output = Up + Reg0;
+    System.out.println(output);
+
+
+
+
   
   }
 
